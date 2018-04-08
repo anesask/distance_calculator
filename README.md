487d478a37d3ee82deb5c63a0a85aef0e2eab976
Anes Mulalić <anes.mulalic@hotmail.com>
2018-04-08 22:14:30 +0200

    First commit...

---
+++ README.md
@@ -0,0 +1,4 @@
+# Distance Calculator - Google Maps Distance Matrix API
+
+Google Maps Distance Calculator<br>
+Simple web app for measuring the driving distance and duration between two cities, based on JavaScript and Google Maps Distance Matrix API.
---
+++ js/main.js
@@ -0,0 +1,81 @@
+var myLatLng = { lat: 0.0, lng: 0.0 };
+var mapOptions = {
+    center: myLatLng,
+    zoom: 1,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+
+// Hide result box
+document.getElementById("output").style.display = "none";
+
+// Create/Init map
+var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+
+// Create a DirectionsService object to use the route method and get a result for our request
+var directionsService = new google.maps.DirectionsService();
+
+// Create a DirectionsRenderer object which we will use to display the route
+var directionsDisplay = new google.maps.DirectionsRenderer();
+
+// Bind the DirectionsRenderer to the map
+directionsDisplay.setMap(map);
+
+
+// Define calcRoute function
+function calcRoute() {
+    //create request
+    var request = {
+        origin: document.getElementById("location-1").value,
+        destination: document.getElementById("location-2").value,
+        travelMode: google.maps.TravelMode.DRIVING,
+        unitSystem: google.maps.UnitSystem.METRIC
+    }
+
+    // Routing
+    directionsService.route(request, function (result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+
+            //Get distance and time            
+            
+            $("#output").html("<div class='result-table'> Driving distance: " + result.routes[0].legs[0].distance.text + ".<br />Duration: " + result.routes[0].legs[0].duration.text + ".</div>");
+            document.getElementById("output").style.display = "block";
+
+            //display route
+            directionsDisplay.setDirections(result);
+        } else {
+            //delete route from map
+            directionsDisplay.setDirections({ routes: [] });
+            //center map in London
+            map.setCenter(myLatLng);
+
+            //Show error message           
+           
+            alert("Can't find road! Please try again!");
+            clearRoute();
+        }
+    });
+
+}
+
+// Clear results
+
+function clearRoute(){
+    document.getElementById("output").style.display = "none";
+    document.getElementById("location-1").value = "";
+    document.getElementById("location-2").value = "";
+    directionsDisplay.setDirections({ routes: [] });
+    
+}
+
+// Create autocomplete objects for all inputs
+
+var options = {
+    types: ['(cities)']
+}
+
+
+var input1 = document.getElementById("location-1");
+var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
+
+var input2 = document.getElementById("location-2");
+var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
